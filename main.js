819cc404058d469d9e7e7dc76c52688d605f8317
Luco <luco@192.168.1.82>
2022-02-03 19:15:20 -0300

    update

--- main.js
+++ main.js
@@ -1,18 +1,57 @@
-function button (x){
-    if(x=="c"){
-        return document.getElementById('display').value = "";
-    }
-    document.getElementById('display').value += x;
-}
+var app = new Vue({
+  el: "#app",
+  data: {
+    header: "Welcome!",
+    message: "Let's organize your day!",
+    seen: true,
+    todoList: [
+      { id: 0, text: "Learn JS" },
+      { id: 1, text: "Learn Vue" },
+      { id: 2, text: "Buy criollitos" },
+    ],
+    dynamicText: "",
+  },
 
-function result (x){
-    var result = eval(document.getElementById('display').value);
-    document.getElementById('display').value = result;
-}
-
-function eraseLastChar (){
-    var string = document.getElementById('display').value;
-    string = string.substring(0, string.length - 1);
-    return document.getElementById('display').value = string;
-}
-
+  methods: {
+    hideButton: function () {
+      this.header = "What are you doing today?";
+      this.message = "My to do list: ";
+      this.seen = false;
+    },
+    addItem: function () {
+      if (this.dynamicText.length == 0) {
+        return;
+      } else {
+        this.todoList.push({
+          id: this.todoList.length,
+          text: this.dynamicText,
+        });
+        this.resetInput();
+      }
+    },
+    resetInput: function () {
+      this.dynamicText = "";
+    },
+    deleteLastItem: function () {
+      this.todoList.splice(-1);
+    },
+    cleanList: function () {
+      this.todoList = [];
+    },
+    deleteItem: function (item) {
+      this.todoList.splice(this.todoList.indexOf(item), 1);
+    },
+    editItem: function (item) {
+      console.log(item);
+      this.todoList;
+      this.dynamicText = this.todoList.indexOf(item);
+      if (this.dynamicText.length == 0) {
+        return;
+      } else {
+        /* console.log(this.todoList.indexOf(item)) */
+        /* var index = this.todoList.indexOf(item);
+                this.todoList. = this.dynamicText; */
+      }
+    },
+  },
+});
--- readme.md
+++ readme.md
@@ -5,4 +5,4 @@ Simple Vue 2 CRUD app I did as an interview for my current job
 # [LINK](https://bakeluco.github.io/vue2-simple-todo)
 
 # Images
-![DEMO](./demo.png)
+![DEMO](./demo.gif)
